b9ceefa610849a7397ecfe6e6b6a6d457b0f0f88
IndeewariAkarawita <indeewariua@gmail.com>
2015-03-11 13:28:12 +0530

    License update

--- src/AcquisitionTop.java
+++ src/AcquisitionTop.java
@@ -1,8 +1,8 @@
 // Copyright 2015 Indeewari Akarawita
 //
-//    This file is a part of Androspy
+//    This file is a part of ANDROPHSY
 //
-//    Androspy is free software: you can redistribute it and/or modify
+//    ANDROPHSY is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
