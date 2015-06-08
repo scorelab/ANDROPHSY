a778b5b29d6d88b5b1a44d0df6cdf786610e575b
swsachith@gmail.com <$Kobebryant24>
2015-06-08 09:11:12 +0530

    added solr querier to IDataModel Solr interface

--- src/main/java/lk/score/androphsy/indexer/IDataModel.java
+++ src/main/java/lk/score/androphsy/indexer/IDataModel.java
@@ -1,9 +1,16 @@
 package lk.score.androphsy.indexer;
 
+import lk.score.androphsy.exceptions.PropertyNotDefinedException;
 import lk.score.androphsy.model.FileMetadata;
+import org.apache.solr.client.solrj.SolrServerException;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface IDataModel {
-    public void addMetadata(FileMetadata metadata);
+    void addMetadata(FileMetadata metadata) throws IOException, SolrServerException, PropertyNotDefinedException;
 
-    public void updateMetadata(FileMetadata metadata);
+    void updateMetadata(FileMetadata metadata) throws IOException, SolrServerException, PropertyNotDefinedException;
+
+    List<FileMetadata> getResultsFromQueryString(String queryString) throws SolrServerException, PropertyNotDefinedException, IOException;
 }
--- src/main/java/lk/score/androphsy/indexer/solr/SolrDataModel.java
+++ src/main/java/lk/score/androphsy/indexer/solr/SolrDataModel.java
@@ -1,17 +1,20 @@
 package lk.score.androphsy.indexer.solr;
 
 import lk.score.androphsy.exceptions.PropertyNotDefinedException;
+import lk.score.androphsy.indexer.IDataModel;
 import lk.score.androphsy.model.FileMetadata;
 import org.apache.solr.client.solrj.SolrServerException;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * This class is used for all the interactions between the system and the SOLR database
  * It supports adding, updating, deleting and querying the database
  */
-public class SolrDataModel {
+public class SolrDataModel implements IDataModel{
     private SolrIndexer indexer;
+    private SolrQuerier querier;
 
     public SolrDataModel() {
         this.indexer = new SolrIndexer();
@@ -36,4 +39,18 @@ public class SolrDataModel {
     public void updateMetadata(FileMetadata fileMetadata) throws IOException, SolrServerException, PropertyNotDefinedException {
         indexer.updateMetadata(fileMetadata);
     }
+
+    /**
+     * This method queries the Solr Database with the given query string
+     * @param queryString
+     * @return List of FileMetadataObjects
+     * @throws SolrServerException
+     * @throws PropertyNotDefinedException
+     * @throws IOException
+     */
+    public List<FileMetadata> getResultsFromQueryString(String queryString) throws SolrServerException, PropertyNotDefinedException, IOException {
+        return querier.querySolr(queryString);
+    }
+
+
 }
